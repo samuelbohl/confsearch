e01b6546de8f83c6d0f731bc6cd5ae60ef1d784d
Giannis Vasileiou <giannisvas02@gmail.com>
2024-10-10 13:59:24 +0300

    add appsettings to git

---
+++ frontend/src/appsettings.ts
@@ -0,0 +1,5 @@
+// const SERVER_URL: string = "http://localhost:5000"
+// const SERVER_URL: string = "http://192.168.2.5:5000"
+const SERVER_URL: string = "http://ee-tik-gpuvm002.ethz.ch"
+
+export default SERVER_URL;
